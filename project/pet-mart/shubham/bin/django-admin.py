90ed28aa13d9310d102b38411894b701cf8de494
Shubham Gapat <shubhamgapat14@gmail.com>
2020-09-08 17:30:02 +0530

    secret key changes

---
+++ project/pet-mart/shubham/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shubham/source/shubham-gapat.github.io/project/pet-mart/shubham/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
